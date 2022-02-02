54e5e02f718f05cf53234c80be288bada64c87cb
Oluwaturheeb <Oluwaturheeb@gmail>
2022-02-03 00:42:31 +0100

    update

--- db.js
+++ db.js
@@ -4,16 +4,8 @@ import dotenv from 'dotenv';
 // setup config
 dotenv.config({path: './conf/config.env'});
   
-const query = await postgres({
-  user: process.env.dbUser,
-  password: process.env.dbPassword,
-  host: process.env.dbHost,
-  port: process.env.dbPort,
-  database: 'customer_tab',
-  ssl: false
-});
-
-// let table = await query `create table users(id bigserial, account bigint not null, name varchar(50))`;
-//let table = await query `create table tab(id bigserial, paid int default 0, total int not null, description text)`;
-
+const query = await postgres(process.env.DATABASE_URL || 'postgres://superuser@localhost:5432/customer_tab');
+// let table = await query ``;
+// let table = await query `create table if not exists users(id bigserial primary key, account bigint not null, name varchar(50));
+// create if not exists table tab(id bigserial, user_id bigint not null, paid int default 0, total int default 0, description text null)`;
 export default query;
